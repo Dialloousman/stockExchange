71b08e89ac27639c0136e11fc6294adc0b5f470f
Dialloousman <47230143+Dialloousman@users.noreply.github.com>
2020-09-16 17:18:46 -0400

    added register html page

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-console.log('assets path--> ', path.resolve(__dirname, "../assets"))
+console.log("assets path--> ", path.resolve(__dirname, "../assets"));
 app.use("/", express.static(path.resolve(__dirname, "../assets")));
 
-app.get("/", (req, res) => {
+app.get("/signin", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../view/signin.html"));
+});
+
+app.get("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../view/signup.html"));
 });
 
 app.listen(port, () => {
